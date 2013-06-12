a4ab685d406112e6a4f5ee5449074631c24a1447
James Duyck <james.duyck@synaptics.com>
2013-06-12 12:10:53 -0700

    longer message again

--- test.c
+++ test.c
@@ -2,5 +2,5 @@
 #include<stdio.h>
 main()
 {
-	printf("Hello Mr. World, nice day we're having isn't it? Nice cap. Shame it's melting.");
+	printf("Hello Mr. World, nice day we're having isn't it? Nice cap. Shame it's melting. I don't think this sentence will show up the first time that I type it.");
 }
